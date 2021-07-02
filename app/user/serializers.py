1c34dbe7b0f2d240cfb4aae4e5c521f157e04d57
clemdetry <c.detry@student.maastrichtuniversity.nl>
2021-07-02 13:26:05 +0200

    response rest api

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -8,19 +8,19 @@ class AuthorSerializer(serializers.ModelSerializer):
         fields = ['first_name', 'last_name', 'email', 'avatar']
 
 
-    def create(self, validated_data):
-        """
-        Create and return a new `User` instance, given the validated data.
-        """
-        return User.objects.create(**validated_data)
+    # def create(self, validated_data):
+    #     """
+    #     Create and return a new `User` instance, given the validated data.
+    #     """
+    #     return User.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
-        """
-        Update and return an existing `User` instance, given the validated data.
-        """
-        instance.first_name = validated_data.get('first_name', instance.first_name)
-        instance.last_name = validated_data.get('last_name', instance.last_name)
-        instance.email = validated_data.get('email', instance.email)
-        instance.author_avatar = validated_data.get('author_avatar', instance.author_avatar)
-        instance.save()
-        return instance
+    # def update(self, instance, validated_data):
+    #     """
+    #     Update and return an existing `User` instance, given the validated data.
+    #     """
+    #     instance.first_name = validated_data.get('first_name', instance.first_name)
+    #     instance.last_name = validated_data.get('last_name', instance.last_name)
+    #     instance.email = validated_data.get('email', instance.email)
+    #     instance.author_avatar = validated_data.get('author_avatar', instance.author_avatar)
+    #     instance.save()
+    #     return instance
